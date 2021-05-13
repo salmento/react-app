e3d021856a57d07c2dec217cc295489b759e1ecb
salmento <salmentochitlango@gmail.com>
2021-05-13 16:08:48 +0200

    Working on css

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// CSS
+import './index.css'
+
 const BookList = () => {
   return (
-      <section> 
+      <section className = 'booklist'> 
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
         <Book/>
       </section>
   )
@@ -11,21 +21,16 @@ const BookList = () => {
 
 const Book = () => {  
   return (
-    <article>
-      <Image/>
-      <Title/>
-      <Author/>
+    <article className = 'book'>
+      <img 
+        src="https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg" 
+      alt = ""/>
+      <h1> American Marxism Hardcover  </h1>
+      <h4> Mark R. Levin </h4>
     </article>
   );
 }
 
-const Image = () => <img 
-  src="https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg" 
-  alt = ""/>
-
-const Author = () =>  <h1> Mark R. Levin </h1>
-
-const Title = () => <h1> American Marxism Hardcover  </h1>
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
 
