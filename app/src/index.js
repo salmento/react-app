20c179fb790296e6221aebf5ec6c3066f6ba42ca
salmento <salmentochitlango@gmail.com>
2021-05-15 02:07:58 +0200

    Working with props

--- app/src/index.js
+++ app/src/index.js
@@ -4,29 +4,34 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css'
 
+// Setup vars
+const firstBook = {
+  title : "American Marxism Hardcover",
+  author: "Mark R. Levin",
+  img : "https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg",
+}
+const secondBook = {
+  title: "Oh, the Places You'll Go! ",
+  author: "Dr. Seuss",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX376_BO1,204,203,200_.jpg",
+}
+
+
 const BookList = () => {
   return (
       <section className = 'booklist'> 
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
+      <Book img= {firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
       </section>
   )
 }
 
-const Book = () => {  
+const Book = ({img, title, author}) => {  
   return (
     <article className = 'book'>
-      <img 
-        src="https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg" 
-      alt = ""/>
-      <h1> American Marxism Hardcover  </h1>
-      <h4> Mark R. Levin </h4>
+      <img src={img} alt = ""/>
+      <h1>{title}</h1>
+      <h4> {author}</h4>
     </article>
   );
 }
