fc4b6eb1e5ab0f74905c20c296af237e60ebbb54
salmento <salmentochitlango@gmail.com>
2021-05-15 02:43:39 +0200

    interact over array

--- app/src/index.js
+++ app/src/index.js
@@ -5,31 +5,32 @@ import ReactDom from 'react-dom';
 import './index.css'
 
 // Setup vars
-const firstBook = {
-  title : "American Marxism Hardcover",
+const Books = [
+{
+  title: "American Marxism Hardcover",
   author: "Mark R. Levin",
-  img : "https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg",
-}
-const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg",
+},
+{
   title: "Oh, the Places You'll Go! ",
   author: "Dr. Seuss",
   img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX376_BO1,204,203,200_.jpg",
 }
-
-
+]
 const BookList = () => {
   return (
-      <section className = 'booklist'> 
-      <Book img= {firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-      </section>
+    <section className='booklist'>
+      {Books.map(book => <Book book={book} />)}
+      
+    </section>
   )
 }
-
-const Book = ({img, title, author}) => {  
+//children is default name to access childrens on props parament
+const Book = (props ) => {
+  const { img, title, author } =props.book;
   return (
-    <article className = 'book'>
-      <img src={img} alt = ""/>
+    <article className='book'>
+      <img src={img} alt="" />
       <h1>{title}</h1>
       <h4> {author}</h4>
     </article>
@@ -37,6 +38,6 @@ const Book = ({img, title, author}) => {
 }
 
 
-ReactDom.render(<BookList/>, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
 
 
