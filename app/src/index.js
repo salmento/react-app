fd8a341c6e70ff3e74a456c3efcd8c5e6bf2ddd5
salmento <salmentochitlango@gmail.com>
2021-05-15 03:19:14 +0200

    using spring operator

--- app/src/index.js
+++ app/src/index.js
@@ -6,28 +6,36 @@ import './index.css'
 
 // Setup vars
 const Books = [
-{
-  title: "American Marxism Hardcover",
-  author: "Mark R. Levin",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg",
-},
-{
-  title: "Oh, the Places You'll Go! ",
-  author: "Dr. Seuss",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX376_BO1,204,203,200_.jpg",
-}
+  { 
+    id:1,
+    title: "American Marxism Hardcover",
+    author: "Mark R. Levin",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51QyOJtK0xS._SX322_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 2,
+    title: "Oh, the Places You'll Go! ",
+    author: "Dr. Seuss",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51VU-ask3QS._SX376_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 3,
+    title: "What Happened to You?: Conversations on Trauma, Resilience, and Healing ",
+    author: "Oprah Winfrey  && Bruce D. Perry",
+    img: "https://images-na.ssl-images-amazon.com/images/I/31LXam5J2IL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  }
 ]
 const BookList = () => {
   return (
     <section className='booklist'>
-      {Books.map(book => <Book book={book} />)}
-      
+      {Books.map(book => <Book key={book.id} {...book} />)}
+
     </section>
   )
 }
 //children is default name to access childrens on props parament
-const Book = (props ) => {
-  const { img, title, author } =props.book;
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt="" />
