45b2fdeb26b5aab21167e2ad9a0ca3d4af6449d3
salmento <salmentochitlango@gmail.com>
2021-05-23 11:25:36 +0200

    Restructure the project

--- src/tutorial/1-useState/1-error-example.js
+++ src/tutorial/1-useState/1-error-example.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { data } from '../../../data';
+import { data } from '../../data';
 
 
 const Example = () => {
--- src/tutorial/6-useReducer/index.js
+++ src/tutorial/6-useReducer/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
-import { data } from '../../../data';
+import { data } from '../../data';
 // reducer function
 import { reducer } from './reducer';
 const defaultState = {
